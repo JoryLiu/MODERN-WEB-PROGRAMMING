9ecd83f9baf89c5bad6ab22bfbe17d5d2587aa9f
JoryLiu <liuzhaorui55@hotmail.com>
2016-12-19 19:37:01 +0800

    update

--- signin/app.js
+++ signin/app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +21,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  store: new FileStore(),
+  resave: false,
+  saveUninitialized: false,
+  secret: 'modern web programming'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- signin/routes/index.js
+++ signin/routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var validator = require('../public/javascripts/validator.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var users = {}
+
+/* GET detail page. */
+router.get('/detail', function(req, res, next) {
+  res.render('detail', { title: 'User Information', user: req.session.user });
+});
+
+/* GET sign up page. */
+router.get('/signUp', function(req, res, next) {
+  res.render('signUp', { title: 'Sign Up', user: {}  });
+});
+
+router.post('/signUp', function(req, res, next) {
+  var user = req.body;
+  try {
+    checkUser(user);
+    req.session.user = users[user.name] = user;
+    res.redirect('/detail');
+  } catch(err) {
+    res.render('signUp', { title: 'Sign Up', user: user, error: err.message });
+  }
+});
+
+router.all('*', function(req, res, next) {
+  req.session.user ? next() : res.redirect('/signin');
 });
 
 module.exports = router;
+
+function checkUser(user) {
+  var errorMessages = [];
+  for (var key in user) {
+    if (user[key] != undefined) {
+      if (!validator.isFieldValid(key, user[key])) errorMessages.push(validator.form[key].errorMessage);
+      if (!validator.isAttrValueUnique(users, user, key)) {
+        error = "key: " + key + ' is not unique with value: ' + user[key];
+        errorMessages.push(error);
+      }
+    }
+  }
+  if (errorMessages.length > 0) throw new Error(errorMessages.join('<br />'));
+}
